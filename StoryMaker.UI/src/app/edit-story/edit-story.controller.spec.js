e76e1365260d3967a17c262bfe96021087ffdf14
david.brower <david.brower@civil.lmco.com>
2015-10-07 17:57:43 +0100

    Updates edit-story specs

--- StoryMaker.UI/src/app/edit-story/edit-story.controller.spec.js
+++ StoryMaker.UI/src/app/edit-story/edit-story.controller.spec.js
@@ -10,11 +10,17 @@
 			expect(vm).toBeDefined();
 		}));
 		
-		it("should have a text and a creator", inject(function($controller){
+		it("should have a text and an author", inject(function($controller){
 			var vm = $controller('EditStoryController');
 			
 			expect(vm.text).toBeDefined();
 			expect(vm.author).toBeDefined();
+		}));
+		
+		it("saveStory() should be defined", inject(function($controller){
+			var vm = $controller('EditStoryController');
+			
+			expect(vm.saveStory).toBeDefined();
 		}));
 		
 		it("saveStory() should be defined", inject(function($controller){
